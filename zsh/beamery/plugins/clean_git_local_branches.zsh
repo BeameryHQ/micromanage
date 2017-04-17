4bdc2a191c4afe547b5cad09cb017cee939df220
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2017-04-17 11:17:29 +0100

    make sure git delete-branch exists without the need for git-extras

--- zsh/beamery/plugins/clean_git_local_branches.zsh
+++ zsh/beamery/plugins/clean_git_local_branches.zsh
@@ -8,9 +8,25 @@ source "${HOME}/.oh-my-zsh/plugins/beamery/pluginsInterface.zsh"
 
 clean_git_local_branches() {
 
-    execute -g $@ "echo""; git branch --merged | egrep -v '(^\*|master|development)'| xargs -I {} git delete-branch {}"
+    delete-branch(){
+        for branch in "$@"
+        do
+          remote=$(git config branch.$branch.remote)
+          test -z $remote && remote="origin"
+          ref=$(git config branch.$branch.merge)
+          test -z $ref && ref="refs/heads/$branch"
+
+          git branch -D $branch
+          git branch -d -r $remote/$branch
+          git push $remote :$ref
+        done
+    }
+
+    execute -g $@ "echo""; git branch --merged | egrep -v '(^\*|master|development)'| xargs -I {} delete-branch {}"
     execute -g $@ "echo""; git remote prune origin"
 
-    printf "\n${YELLOW}Making sure that all 'gone' branches are also removed ..\n\n${NC}"
-    execute -g $@ "git fetch -p && for branch in `git branch -vv | grep ': gone]' | gawk '{print $1}'`; do git branch -D $branch; done"
+    if [[ `git branch -vv | grep ': gone]'` ]]; then
+        printf "\n${YELLOW}Making sure that all 'gone' branches are also removed ..\n\n${NC}"
+        execute -g $@ "git fetch -p && for branch in `git branch -vv | grep ': gone]' | gawk '{print $1}'`; do git branch -D $branch; done"
+    fi
 }
