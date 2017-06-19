bf9d5e7ffeaa0117be41c8cd8923ca2ded44cac8
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2017-06-19 18:54:28 +0100

    adding documentation for check_npm_usage

--- beamery/plugins/check_npm_usage.sh
+++ beamery/plugins/check_npm_usage.sh
@@ -8,7 +8,6 @@ source "${HOME}/.bash_it/plugins/available/beamery/pluginsInterface.bash"
 
 check_npm_usage() {
 
-  # Save and keep track of the parent directory to revert back to after operations are done
   PARENT_FOLDER=`pwd`
 
   find . -maxdepth 1 -type d \( ! -name . \) | while read -r SUBFOLDER; do [[ -f "$PARENT_FOLDER/$SUBFOLDER/package.json" ]] && cat "$PARENT_FOLDER/$SUBFOLDER/package.json" | grep -sw $1 && echo "$SUBFOLDER"; done;
