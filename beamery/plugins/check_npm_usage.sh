dd06639f5219eb99ec197213b600ca2e2446060e
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2017-06-20 11:00:42 +0100

    update check_npm_usage to do exact package match

--- beamery/plugins/check_npm_usage.sh
+++ beamery/plugins/check_npm_usage.sh
@@ -10,6 +10,6 @@ check_npm_usage() {
 
   PARENT_FOLDER=`pwd`
 
-  find . -maxdepth 1 -type d \( ! -name . \) | while read -r SUBFOLDER; do [[ -f "$PARENT_FOLDER/$SUBFOLDER/package.json" ]] && cat "$PARENT_FOLDER/$SUBFOLDER/package.json" | grep -sw $1 && echo "$SUBFOLDER"; done;
+  find . -maxdepth 1 -type d \( ! -name . \) | while read -r SUBFOLDER; do [[ -f "$PARENT_FOLDER/$SUBFOLDER/package.json" ]] && cat "$PARENT_FOLDER/$SUBFOLDER/package.json" | grep -sw "\"$1\":" && echo "$SUBFOLDER"; done;
 
 }
